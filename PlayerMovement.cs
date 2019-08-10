4e8ed481717757a0acf08abf3a4841b5dbcaf11c
IrateWarbler <dorianparaschiv96@gmail.com>
2019-08-10 18:05:50 +0300

    second commit

--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -6,8 +6,13 @@ public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb2d;
 
+    private int currentJumps = 1;
+    private int defaultJumps = 1;
+
     public float speed = 1.0f;
     public float jump = 20.0f;
+
+    public Animator animator;
 
     private void Awake()
     {
@@ -41,14 +46,36 @@ public class PlayerMovement : MonoBehaviour
 
         rb2d.velocity = new Vector2(speed * Input.GetAxis("Horizontal"), rb2d.velocity.y);
 
-        
+        if (Mathf.Abs(rb2d.velocity.x) > 0)
+        {
+            animator.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
+        }
+        else
+        {
+            animator.SetFloat("Speed", Mathf.Abs(rb2d.velocity.x));
+        }
+
+        if (rb2d.velocity.normalized.x > 0 && transform.localScale.x < 0)
+            transform.localScale = new Vector3(-1 * transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        if (rb2d.velocity.normalized.x < 0 && transform.localScale.x > 0)
+            transform.localScale = new Vector3(-1 * transform.localScale.x, transform.localScale.y, transform.localScale.z);
+
+        animator.SetFloat("VerticalSpeed", rb2d.velocity.normalized.y);
+
+        if (Input.GetAxis("Vertical") > 0 && currentJumps > 0)
+        {
+            rb2d.AddForce(new Vector2(0, jump));
+            currentJumps--;
+        }
+
     }
+
 
     private void OnCollisionStay2D(Collision2D collision)
     {
-        if (collision.gameObject.name == "Platform" && Input.GetAxis("Vertical") > 0)
+        if (collision.gameObject.tag == "Platform")
         {
-            rb2d.AddForce(new Vector2(0, jump));
+            currentJumps = defaultJumps;
         }
     }
 }
