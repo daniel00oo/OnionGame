59b1370adace63125412a79aea28278faf33d63a
daniel00oo <madalinadaniel60@gmail.com>
2019-08-13 21:48:59 +0300

    New collision system

---
+++ Movement2D.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Movement2D : MonoBehaviour
+{
+
+    const float skinwidth = 0.015f;
+    BoxCollider2D col;
+    RaycastOrigins rays;
+    public CollisionInfo collisions;
+
+    public int horizontalRays = 2, verticalRays = 2;
+    public LayerMask layerCollision;
+
+    float horizontalRaySpacing, verticalRaySpacing;
+
+    private void Start()
+    {
+        col = GetComponent<BoxCollider2D>();
+        CalculateRaySpacing();
+    }
+
+    void CalculateRaySpacing()
+    {
+        Bounds bounds = col.bounds;
+        bounds.Expand(skinwidth * -2);
+
+        horizontalRays = Mathf.Clamp(horizontalRays, 2, int.MaxValue);
+        verticalRays = Mathf.Clamp(verticalRays, 2, int.MaxValue);
+
+        horizontalRaySpacing = bounds.size.y / (horizontalRays - 1);
+        verticalRaySpacing = bounds.size.x / (verticalRays - 1);
+
+    }
+
+    public void Move(Vector3 velocity)
+    {
+        collisions.Reset();
+        UpdateRaycasts();
+        //if (velocity.x != 0)
+            HorizontalCollisions(ref velocity);
+        //if (velocity.y != 0)
+            VerticalCollisions(ref velocity);
+
+        transform.Translate(velocity);
+    }
+
+    void VerticalCollisions(ref Vector3 velocity)
+    {
+        float directionY = Mathf.Sign(velocity.y);
+        float rayLength = Mathf.Abs(velocity.y) + skinwidth;
+
+        for(int i = 0; i < verticalRays; i++)
+        {
+            Vector2 rayOrigin = (directionY == -1) ? rays.bottomleft : rays.topleft;
+            rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
+
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, layerCollision);
+
+            Debug.DrawRay(rayOrigin, Vector2.up * directionY * rayLength, Color.red);
+
+            if (hit)
+            {
+
+                velocity.y = (hit.distance - skinwidth) * directionY;
+                rayLength = hit.distance;
+                
+                collisions.below = directionY == -1;
+                collisions.above = directionY == 1;
+            }
+        }
+    }
+
+    void HorizontalCollisions(ref Vector3 velocity)
+    {
+        float directionX = Mathf.Sign(velocity.x);
+        float rayLength = Mathf.Abs(velocity.x) + skinwidth;
+
+        for (int i = 0; i < horizontalRays; i++)
+        {
+            Vector2 rayOrigin = (directionX == -1) ? rays.bottomleft : rays.bottomright;
+            rayOrigin += Vector2.up * horizontalRaySpacing * i;
+
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, layerCollision);
+
+            Debug.DrawRay(rayOrigin, Vector2.right * directionX * rayLength, Color.red);
+
+            if (hit)
+            {
+                velocity.x = (hit.distance - skinwidth) * directionX;
+                rayLength = hit.distance;
+                
+                collisions.left = directionX == -1;
+                collisions.right = directionX == 1;
+            }
+        }
+    }
+
+    void UpdateRaycasts()
+    {
+        Bounds bounds = col.bounds;
+        bounds.Expand(skinwidth * -2);
+
+        rays.bottomleft = new Vector2(bounds.min.x, bounds.min.y);
+        rays.bottomright = new Vector2(bounds.max.x, bounds.min.y);
+        rays.topleft = new Vector2(bounds.min.x, bounds.max.y);
+        rays.topright = new Vector2(bounds.max.x, bounds.max.y);
+    }
+
+    struct RaycastOrigins
+    {
+        public Vector2 topleft, topright;
+        public Vector2 bottomleft, bottomright;
+    }
+
+    public struct CollisionInfo
+    {
+        public bool above, below;
+        public bool left, right;
+
+        public void Reset()
+        {
+            above = below = false;
+            left = right = false;
+        }
+    }
+
+}
---
+++ Player.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (Movement2D))]
+public class Player : MonoBehaviour
+{
+    public float jumpHeight = 4f;
+    public float jumptimeApex = .4f;
+    public float moveSpeed = 6;
+    float gravity;
+    Vector3 velocity;
+
+    public KeyCode jumpKey;
+    public int nrOfJumps;
+    private int currentJumpCount;
+    float jumpVelocity = 8;
+
+    private bool prevBelow;
+
+    Movement2D controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gravity = -(2 * jumpHeight) / Mathf.Pow(jumptimeApex, 2);
+        jumpVelocity = -gravity * jumptimeApex;
+        controller = GetComponent<Movement2D>();
+        jumpKey = KeyCode.Space;
+        currentJumpCount = nrOfJumps;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.collisions.left)
+        {
+            Debug.Log(true);
+        }
+        //Debug.Log(controller.collisions.below);
+        if (prevBelow == true && controller.collisions.below == false && currentJumpCount == nrOfJumps)
+        {
+            prevBelow = false;
+            currentJumpCount--;
+        }
+        if (controller.collisions.above || controller.collisions.below)
+            velocity.y = 0;
+        if (controller.collisions.below)
+            currentJumpCount = nrOfJumps;
+
+        if (Input.GetKey(jumpKey) && currentJumpCount > 0)
+        {
+            currentJumpCount--;
+            velocity.y = jumpVelocity;
+        }
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        velocity.x = input.x * moveSpeed;
+
+        velocity.y += Time.deltaTime * gravity;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        if (controller.collisions.below)
+            prevBelow = true;
+    }
+}
